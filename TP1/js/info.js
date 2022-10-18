a6917968c8ffa47fde1b9eaedc6f60cf80bc619c
gerardopizzi1 <112650818+gerardopizzi1@users.noreply.github.com>
2022-10-18 07:44:47 -0300

    Add files via upload

--- TP1/js/info.js
+++ TP1/js/info.js
@@ -8,8 +8,8 @@ si luego se achica mas la pantalla a menos de 800px la letra del texto cambia a 
 si luego se achica mas la pantalla a menos de 600px la letra cambia a color negro y negrita.
 En el Título de la Página Inicio Restaurant "Los Amigos" se utilizó un tipo de Letra de Google Fonts.
 Los Iconos de las Páginas Menú 1 y Menú 2 que ilustran las opciones son de FontsAwesome.
-En el body se coloco un iframe de Google Maps. 
-En la página Menú 1 se colocaron Imágenes de los diferentes Platos Principales con Vue y presionando el botón cambian las imágenes.
-En la página Menú 2 se colocaron Imágenes de los diferentes Postres con Vue y presionando el botón cambian las imágenes.
-En la página del Formulario cuando se presiona el boton Enviar se muestran los datos enviados.
+En el body de la Página Inicio se coloco un iframe de Google Maps. 
+En la Página Menú 1 se colocaron Imágenes de los diferentes Platos Principales con Vue y presionando el botón cambian las imágenes.
+En la Página Menú 2 se colocaron Imágenes de los diferentes Postres con Vue y presionando el botón cambian las imágenes.
+En la Página del Formulario cuando se presiona el boton Enviar se muestran los datos enviados.
 */
