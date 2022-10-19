c77250585b3024b86168e8f9fc8ee4d1aae24e6c
gerardopizzi1 <112650818+gerardopizzi1@users.noreply.github.com>
2022-10-19 09:54:21 -0300

    Add files via upload

---
+++ info.js
@@ -0,0 +1,16 @@
+/* 
+El sitio consta de 4 páginas, posee una estructura html maquetada con Grid
+La Página Inicio posee una animación con una barra de deslizamiento vertical.
+La Página Inicio posee 3 puntos de corte en 1000px 800px y 600 px. 
+Para Pantallas de mas de 1000px el body se distribuye en 3 Columnas.
+Cuando el ancho se achica a menos de 1000px la publicidad de la 3ra columna baja mostrando el body en 2 columnas, 
+si luego se achica mas la pantalla a menos de 800px la letra del texto cambia a color rojo y oblicua, 
+si luego se achica mas la pantalla a menos de 600px la letra cambia a color negro y negrita.
+En el Título de la Página Inicio Restaurant "Los Amigos" se utilizó un tipo de Letra de Google Fonts.
+Los Iconos de las Páginas Menú 1 y Menú 2 que ilustran las opciones son de FontsAwesome.
+En el body de la Página Inicio se coloco un iframe de Google Maps. 
+En la Página Menú 1 se colocaron 6 Imágenes con Vue de los diferentes Platos Principales y presionando el botón cambian las imágenes.
+En la Página Menú 2 se colocaron 6 Imágenes con Vue de los diferentes Postres y presionando el botón cambian las imágenes.
+Cuando se acerca el mouse a las imágenes muestra el Nro de Plato o el Nro de Postre.
+En la Página del Formulario cuando se presiona el boton Enviar se muestran los datos enviados.
+*/
