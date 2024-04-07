bc885493a5bb01165d5c0f089f87c3d5df4c4966
gerardopizzi1 <112650818+gerardopizzi1@users.noreply.github.com>
2024-04-07 11:24:58 -0300

    Update vue.js

--- TP1/js/vue.js
+++ TP1/js/vue.js
@@ -1,46 +1,41 @@
 
-    const {createApp} = Vue
-    const miApp1 = createApp({
-        data() {
-            return {
-                titulo: "",                            
-                indice: 1,                             
-                imagen: "img/Plato" + this.indice + ".jpg",
-                ultimaImagen: 6,
-                textoBoton: "Cambiar imagen",                 
-                alto: 300,                            
+const {createApp} = Vue;
+const MiApp1 = createApp({
+    data() {
+        return {
+            titulo: "",
+            indice: 1,
+            imagen: "",
+            ultimoIndice: 6,
+            textoBoton: "Cambiar imagen",
+            alto: 300,
+        };
+    },
+    mounted() {
+        this.imagen = "img/Plato" + this.indice + ".jpg";
+        this.titulo = "Foto: " + this.indice;
+    },
+    methods: {
+        cambiarImagen() {
+            if (this.indice < this.ultimoIndice) {
+                this.indice++;
+            } else {
+                this.indice = 1;
             }
-        },             
-        
-        mounted() {
-            this.imagen = "img/Plato" + this.indice + ".jpg"     
-            this.titulo = "Foto: " + this.imagen  
+            this.imagen = "img/Plato" + this.indice + ".jpg";
+            this.titulo = "Foto: " + this.indice;
         },
-                
-        methods: {
-                    cambiarImagen() {
-                       
-                        if (this.indice < this.ultimaImagen) {
-                            this.indice++   
-                        } else {
-                            this.indice = 1  
-                        }
-                        this.imagen = "img/Plato" + this.indice + ".jpg"   
-                        this.titulo = "Foto: " + this.imagen 
-                    }
-                }
-
-        }).mount('#app1')
-            
+    },
+}).mount('#app1');
 
     const {createMiApp} = Vue
-    const miApp2 = createApp({
+    const MiApp2 = createApp({
         data() {
             return {
                 titulo: "",                            
                 indice: 1,                             
-                imagen: "img/Postre" + this.indice + ".jpg", 
-                ultimaImagen: 6,                       
+                imagen: "", 
+                ultimoIndice: 6,                       
                 textoBoton: "Cambiar imagen",
                 alto:300, 
             }
@@ -48,20 +43,20 @@
            
         mounted() {
             this.imagen = "img/Postre" + this.indice + ".jpg"     // Ruta y nombre de la foto
-            this.titulo = "Foto: " + this.imagen  // Texto del tooltip
+            this.titulo = "Foto: " + this.indice;  // Texto del tooltip
         },
             
     
         methods: {
                     cambiarImagen() {
                            
-                        if (this.indice < this.ultimaImagen) {
+                        if (this.indice < this.ultimoIndice) {
                             this.indice++   
                         } else {
                             this.indice = 1  
                         }
                         this.imagen = "img/Postre" + this.indice + ".jpg"   
-                        this.titulo = "Foto: " + this.imagen 
+                        this.titulo = "Foto: " + this.indice; 
                     }
                 }
            
